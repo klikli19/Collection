415c894bda48d6c02f368b09277a1ece733e367b
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-10-19 22:07:24 +0300

    дз9 сделала дз1 -2

--- src/HW9/task1_2/Main.java
+++ src/HW9/task1_2/Main.java
@@ -2,31 +2,50 @@ package HW9.task1_2;
 
 import java.util.HashSet;
 import java.util.Set;
-import HW9.task1_2.Product;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ProductException {
 
-        Set<Product> product1 = new HashSet<>();
-        product1.add(new Product("Колбаса", 250.0, 0.5));
-        product1.add(new Product("Сыр", 170.0, 0.25));
-        product1.add(new Product("Огурцы", 100.0, 0.3));
-        product1.add(new Product("Помидоры", 90.0, 0.4));
-        product1.add(new Product("Картофель", 50.0, 0.6));
-        product1.add(new Product("Горошек", 60.0, 0.3));
-        product1.add(new Product("Яйца", 85.0, 0.55));
 
-        Set<Product> product2 = new HashSet<>();
-        product2.add(new Product("Макарошки", 70.0, 0.4));
-        product2.add(new Product("Морковь", 40.0, 0.15));
-        product2.add(new Product("Фарш", 255.0, 0.4));
-        product2.add(new Product("Лук", 30.0, 0.1));
+        Product kolbasa = new Product("Колбаса", 250.0, 0.5);
+        Product cheese = new Product("Сыр", 170.0, 0.25);
+        Product cucumbers = new Product("Огурцы", 100.0, 0.3);
+        Product tomatoes = new Product("Помидоры", 90.0, 0.4);
+        Product potato = new Product("Картофель", 50.0, 0.6);
+        Product peas = new Product("Горошек", 60.0, 0.3);
+        Product eggs = new Product("Яйца", 85.0, 0.55);
 
-        Set<Recipes> recipes1 = new HashSet<>();
-        recipes1.add(new Recipes("Оливьеха",  product1));
-        recipes1.add(new Recipes("Макароны по-флотски", product2));
-        recipes1.add(new Recipes("Салат",  product1));
-        System.out.println(recipes1);
+        ProductList product1 = new ProductList();
+        product1.addProduct(potato);
+        product1.addProduct(kolbasa);
+        product1.addProduct(peas);
+        product1.addProduct(tomatoes);
+        product1.addProduct(cheese);
+//        System.out.println(product1);
+//        product1.delProduct(peas);
+//        System.out.println(product1);
+
+        Product pasta = new Product("Макарошки", 70.0, 0.4);
+        Product carrot = new Product("Морковь", 40.0, 0.15);
+        Product groundMeat = new Product("Фарш", 255.0, 0.4);
+        Product onion = new Product("Лук", 30.0, 0.1);
+        ProductList product2 = new ProductList();
+        product2.addProduct(pasta);
+        product2.addProduct(carrot);
+        product2.addProduct(groundMeat);
+        product2.addProduct(onion);
+
+
+        Recipes recipes = new Recipes("Салат", product1);
+        Recipes recipes0 = new Recipes("Салат", product2);
+        Recipes recipes1 = new Recipes("Макарошки по-флотски", product2);
+        RecipesBook recipesBook = new RecipesBook();
+        recipesBook.addRecipes(recipes);
+        recipesBook.addRecipes(recipes1);
+        recipesBook.addRecipes(recipes0);
+        System.out.println(recipesBook);
+
+
 
 
 
--- src/HW9/task1_2/Product.java
+++ src/HW9/task1_2/Product.java
@@ -9,20 +9,16 @@ public class Product {
     private final String productsName;
     private double price;
     private Double weight;
-    private Set<Product> productSet;
 
 
-    public Product(String productsName, Double price, Double weight) {
+    public Product(String productsName, Double price, Double weight) throws ProductException {
         if (productsName == null || productsName.isBlank()) {
-            throw new RuntimeException("Заполните карточку товара полностью");
+            throw new ProductException("Заполните карточку товара полностью");
         } else {
             this.productsName = productsName;}
         this.price = price;
         this.weight = weight;
-        this.productSet = new HashSet<>();
     }
-
-
     public String getProductsName() {
         return productsName;
     }
@@ -31,13 +27,14 @@ public class Product {
         return price;
     }
 
-    public void setPrice(Double price) {
+    public void setPrice(Double price) throws ProductException {
         if (price == null || price < 0) {
-            throw new RuntimeException("Заполните карточку товара полностью");
+            throw new ProductException("Заполните карточку товара полностью");
         } else {
             this.price = price;
         }
     }
+
 
     public double getWeight() {
         return weight;
@@ -51,11 +48,12 @@ public class Product {
         }
     }
 
-
     @Override
     public boolean equals(Object o) {
-        throw new RuntimeException("Есть такой продукт в списке покупок");
-
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 && Objects.equals(productsName, product.productsName) && Objects.equals(weight, product.weight);
     }
 
     @Override
@@ -65,8 +63,7 @@ public class Product {
 
     @Override
     public String toString() {
-        return "{ Список продуктов:" +
-                "наименование = " + productsName + '\'' +
+        return "{" + productsName +
                 ", цена = " + price +
                 ", вес = " + weight +
                 '}' + "\n";
